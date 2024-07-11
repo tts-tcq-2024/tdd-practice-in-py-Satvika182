9dd0be47da90dd9e059b9c237ce99ec7519cca78
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-11 18:00:43 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,14 +1,11 @@
 def add(numbers: str) -> int:
-    return sum(
-        map(
-            int,
-            filter(
-                lambda x: x.isdigit() and int(x) <= 1000,
-                numbers[2:].split('\n', 1)[-1].replace('\n', numbers[2:].split('\n', 1)[0]).split(numbers[2:].split('\n', 1)[0])
-            )
-        )
-    ) if numbers.startswith('//') else sum(map(int, filter(lambda x: x.isdigit() and int(x) <= 1000, numbers.replace('\n', ",").split(","))))
-
+    if numbers.startswith('//'):
+        delimiter, numbers = numbers[2:].split('\n', 1)
+    else:
+        delimiter = ','
+    
+    num_list = numbers.replace('\n', delimiter).split(delimiter)
+    return sum(int(num) for num in num_list if num.isdigit() and int(num) <= 1000)
 
 
 
