f1d016295e1fcdb4d36cfa540cadc68807c082d1
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-11 16:52:47 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,9 +1,1 @@
-def add(numbers: str) -> int:
-    delimiter = ','
-    numbers_part = numbers
-    
-    if numbers.startswith('//'):
-        delimiter, numbers_part = numbers[2:].split('\n', 1)
-        
-    all_numbers = numbers_part.replace('\n', delimiter).split(delimiter)
-    return sum(int(num) for num in all_numbers if num and int(num) <= 1000)
+
