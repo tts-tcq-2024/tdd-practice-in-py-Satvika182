d074a55c01b0c097ffa44eca87fc64ca5dd48bc9
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-11 14:01:13 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -3,20 +3,8 @@ def add(numbers: str) -> int:
         return 0
     
     delimiter = ','
-    
     if numbers.startswith('//'):
-        parts = numbers.split('\n', 1)
-        delimiter = parts[0][2:]
-        numbers = parts[1]
+        delimiter, numbers = re.split('\n', numbers[2:], 1)
     
-    numbers = numbers.replace('\n', delimiter)
-    num_list = numbers.split(delimiter)
-    
-    total = 0
-    for num in num_list:
-        n = int(num)
-        if n <= 1000:
-            total += n
-    
-    return total
-
+    num_list = re.split(f'[{re.escape(delimiter)}\n]', numbers)
+    return sum(int(num) for num in num_list if int(num) <= 1000)
