e52783db61dc8328d6e47dbfef37742a22946a3b
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-11 17:14:28 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,12 +2,15 @@ def add(numbers: str) -> int:
     if numbers == "":
         return 0
     
-    # Split numbers by comma and convert them to integers
+    # Split numbers by comma and convert them to integers, ignoring numbers greater than 1000
     num_list = numbers.split(',')
     total_sum = 0
     for num in num_list:
-        total_sum += int(num)
+        num_int = int(num)
+        if num_int <= 1000:
+            total_sum += num_int
         
     return total_sum
+
     
 
